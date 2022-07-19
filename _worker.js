80256193993067bbd0c413a545fcf92b0ce33f0f
Tirth Patel <tirrrthpatel@gmail.com>
2022-07-19 04:02:09 -0400

    chore: update worker

--- _worker.js
+++ _worker.js
@@ -20,6 +20,7 @@ export default {
         // console.log(import_node_fs10.Dir)
         const location = new URL(request.url)
         const { origin, host, pathname } = location
+        console.log('host =', host)
         console.log('pathname =', pathname)
         const isStaticAsset = staticAssets.some(name => pathname.endsWith(name))
         const assetPath = `${origin}/${env.ASSETS_DIRECTORY}${pathname}`
@@ -27,14 +28,11 @@ export default {
         if (isStaticAsset) return env.ASSETS.fetch(assetPath)
         if (redirect(location)) return redirect(location)
         // const space = null
-        // const space = await env.SPACES.get(host)
-        const before = Date.now()
-        console.log('before =', before)
-        await env.SPACES.get(host)
-        const after = Date.now()
-        console.log('after =', after)
+        // const space = '5dd1b315-a596-4ddf-b8e9-1ff93799f6f0'
+        const before = new Date().getTime()
+        const space = await env.SPACES.get(host)
+        const after = new Date().getTime()
         console.log('difference =', after - before)
-        const space = '5dd1b315-a596-4ddf-b8e9-1ff93799f6f0'
         console.log('space =', space)
         if (!space) return new Response(null, { status: 404 })
         const cache = caches.default
@@ -45,7 +43,7 @@ export default {
         if (cachedResponse) return cachedResponse
         const html = await fetch(`${env.NEXT_SERVER_ENDPOINT}/api/revalidate?path=${pathname}&space=${space}`).then(res => res.text())
         const response = new Response(html, { status: 200, headers: { 'Content-Type': 'text/html' } })
-        response.headers.set('FC-Space-ID', space)
+        response.headers.set('FC-Space', space)
         response.headers.append('Cache-Control', `max-age=${env.PAGE_REVALIDATE_TIME}`)
         await cache.put(cacheKey, response.clone())
         return response
